d1a7f17bce1e45759bee03d60debe915c735ff46
upraityharshit <you@example.com>
2024-04-23 22:54:41 +0530

    Second commit

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,36 @@
+console.log("this is script file");
+
+const togglesidebar = () =>{
+	if($(".sidebar").is(":visible")){
+		//hide the sidebar
+		$(".sidebar").css("display","none");
+		$(".content .menubtn").css("display","block");
+		$(".content").css("margin-left","0%");
+	}
+	else{
+		//show the sidebar
+		$(".sidebar").css("display","block");
+		$(".content .menubtn").css("display","none");
+		$(".content").css("margin-left","20%");
+	}
+};
+
+function deleteContact(cid) {
+			swal({
+				title: "Are you sure?",
+				text: "Once deleted, you will not be able to recover this Contact!",
+				icon: "warning",
+				buttons: true,
+				dangerMode: true,
+			})
+				.then((willDelete) => {
+					if (willDelete) {
+						swal("Poof! Your imaginary file has been deleted!", {
+						      icon: "success",
+						    });
+						
+					} else {
+						swal("Your contact is safe!");
+					}
+				});
+		}
